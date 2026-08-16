c53558cea9e5661f50af6386cc80010465b781cc
Trouve-Me <133173801+Trouve-Me@users.noreply.github.com>
2026-08-17 01:23:31 +0200

    Add match rule for Knowunity user script

--- Knowunity - PDF Downloader from cards.user.js
+++ Knowunity - PDF Downloader from cards.user.js
@@ -7,6 +7,7 @@
 // @description  Adds a functional “PDF” tag to each results card so the document can be downloaded
 // @author       TrouveMe
 // @include      /^https:\/\/knowunity\.[^/]+\/.*$/
+// @match        *://*knowunity.*/*
 // @grant        none
 // @license MIT
 // ==/UserScript==
